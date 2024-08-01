cc4996fae01850413bee10e223498c212f536e39
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-08-01 14:07:02 +0200

    Added default None for pop

--- src/fairmat_readers_xrd/readers.py
+++ src/fairmat_readers_xrd/readers.py
@@ -278,7 +278,7 @@ def read_bruker_brml(file_path: str, logger: 'BoundLogger' = None) -> Dict[str, 
     scan_type = detect_scan_type(scan_data)
     modified_scan_data = modify_scan_data(scan_data, scan_type)
     modified_scan_data['Omega'] = modified_scan_data.pop(
-        'Theta'
+        'Theta', None
     )  # theta and omega are synonymous in .brml
 
     return {
